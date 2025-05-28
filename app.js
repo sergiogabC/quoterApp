994acd8e697f71950b28845da2da49247c9cf744
gabriel <collantesloartesergiogabriel@gmail.com>
2025-05-28 16:35:19 -0500

    definimos tecnologias

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 import express, { json } from "express";
-import  from 
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT ?? 1234;
 const app = express();
 
+app.use(express.static("view"));
+
 app.get("/", (req, res) => {
   console.log(__dirname);
-  res.send("hola");
+  res.sendFile(path.join(__dirname, "view/indextp.html"));
 });
 
 app.listen(PORT, () => {
