691f7bdc9fb09239e474b4d37e961c1d135b5c56
gabriel <collantesloartesergiogabriel@gmail.com>
2025-06-10 15:27:37 -0500

    comienzo de escuchar el evento

--- view/js/main.js
+++ view/js/main.js
@@ -1,1 +1,6 @@
- 
+// Mostrar Parametros(renderizar el include)
+const btnMostrarParametros = document.getElementById("MostrarParametros");
+
+btnMostrarParametros.addEventListener("click", (e) => {
+  console.log("2");
+});
